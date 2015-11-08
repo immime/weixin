ddcd5701268bde39d20132875efd407e97573458
julytian <971176857@163.com>
2015-11-08 22:54:42 +0800

    Update index.js

--- index.js
+++ index.js
@@ -222,7 +222,7 @@ $(function() {
     }
 
     function showLoading() {
-        var arr = ['img3.jpg', 'img4.jpg', , 'img5.jpg', 'img6.jpg', 'img7.jpg'];
+        var arr = ['img3.jpg', 'img4.jpg', 'img5.jpg'];
         var num = 0;
         $.each(arr, function(i, imgSrc) {
             var imgObj = new Image();
